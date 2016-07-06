49a732eda4a551b07cbaccb440dbbae1cf57dac0
Aniela <anielaamy@gmail.com>
2016-07-05 23:42:26 -0300

    Added css logic and html logic. Working on using chico ui.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,64 +1,71 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dist: {
-        options:{
-          style:'compressed'
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'css/style.css': 'scss/style.scss',
+                    'css/tabs.css': 'scss/tabs.scss'
+                }
+            }
         },
-        files: {
-          'css/style.css' : 'scss/style.scss'
-        }
-      }
-    },
-    autoprefixer:{
-      dist:{
-        files:{
-          'css/style.css':'css/style.css'
-        }
-      }
-    },
-	cssmin: {
-	  options: {
-		shorthandCompacting: false,
-		roundingPrecision: -1
-	  },
-	  target: {
-		files: {
-		  'output/vendor.min.css': 'css/style.css'
-		}
-	  }
-	},
-    watch: {
-      css: {
-        files: 'scss/*.scss',
-        tasks: ['sass', 'autoprefixer']
-      }
-    },
-	uglify: {
-      my_target: {
-        files: {
-          'output/vendor.min.js': 'scripts/script.js'
-        }
-      }
-    },
-	htmlmin : {
-        dist : {
-            options : {
-                removeComments : true,
-                collapseWhitespace : true
+        autoprefixer: {
+            dist: {
+                files: {
+                    'css/style.css': 'css/style.css',
+                    'css/tabs.css': 'css/tabs.css'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
             },
-            files : {
-                'output/index.html' : 'html/index.html'
+            target: {
+                files: {
+                    'dist/vendor.min.css': 'css/*.css'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'scss/*.scss',
+                tasks: ['sass', 'autoprefixer', 'cssmin']
+            },
+            script: {
+                files: 'scripts/*.js',
+                tasks: ['uglify']
+            },
+            html: {
+                files: 'src/*.html',
+                tasks: ['minifyHtml']
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'dist/vendor.min.js': ['scripts/chico.min.js', 'scripts/script.js']
+                }
+            }
+        },
+        minifyHtml: {
+            options: {
+                cdata: true
+            },
+            dist: {
+                files: {
+                    'dist/index.html': 'src/index.html'
+                }
             }
         }
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.registerTask('default',['watch', 'uglify', 'htmlmin']);
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-minify-html');
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('dev', ['sass', 'autoprefixer', 'cssmin', 'uglify', 'minifyHtml']);
 }
---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+//var carousel = $(".myCarousel").carousel({"pagination": true});
+
+//var quantityDropdown = new ch.Dropdown(document.querySelector('.quantity-dropdown'), { 'skin': true });
+
+/*$(window).on("load", function () {
+    //var dropdown3 = $(".quantity-dropdown").dropdown({'skin': true});
+    //var dropdown1 = $(".quantity-dropdown").dropdown();
+    var quantityDropdown = new ch.Dropdown(document.querySelector('.quantity-dropdown'), { 'skin': true });
+});*/
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    var quantityDropdown = new ch.Dropdown(document.querySelector('.quantity-dropdown'), { 'skin': true });
+});
