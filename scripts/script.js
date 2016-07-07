fd454b240bb375d36f5b0b7f82268a0554679ce4
Aniela <anielaamy@gmail.com>
2016-07-07 01:23:15 -0300

    Added aria.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,4 @@
-//var carousel = $(".myCarousel").carousel({"pagination": true});
 
-//var quantityDropdown = new ch.Dropdown(document.querySelector('.quantity-dropdown'), { 'skin': true });
-
-/*$(window).on("load", function () {
-    //var dropdown3 = $(".quantity-dropdown").dropdown({'skin': true});
-    //var dropdown1 = $(".quantity-dropdown").dropdown();
-    var quantityDropdown = new ch.Dropdown(document.querySelector('.quantity-dropdown'), { 'skin': true });
-});*/
-
-/*document.addEventListener('DOMContentLoaded', function(e) {
-	var options = [1,2,3,4,5,6,7,8,9,10];
-	var dropdown = new ch.Dropdown($document.querySelector('.quantity-dropdown'), options);
-    //var quantityDropdown = new ch.Dropdown(document.querySelector('.quantity-dropdown'), { 'skin': true });
-	
-	
-});*/
-
+var quantityDropdown = $("#payment-dropdown").dropdown();
 var quantityDropdown = $("#quantity-dropdown").dropdown();
 var otherPhotos = $("#other-photos").carousel();
